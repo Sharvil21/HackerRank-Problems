d00dc4669c6776a23684523924705358ae70ee47
Sharvil21 <sshastri2102@gmail.com>
2025-01-14 17:07:37 -0500

    Added the first solution

--- Solutions/List_Comprehensions.py
+++ Solutions/List_Comprehensions.py
@@ -1,1 +1,14 @@
+#1st Solution
 
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    ans = []
+    for a in range(0,x+1):
+        for b in range(0,y+1):
+            for c in range(0,z+1):
+                if a+b+c != n:
+                    ans.append([a,b,c])
+    print(ans)
