cd511ef3079636964f350b5850f6094eef0f77af
Sharvil21 <sshastri2102@gmail.com>
2025-02-14 10:51:01 -0500

    Added the 2nd solution

--- Solutions/Mutations.py
+++ Solutions/Mutations.py
@@ -4,3 +4,16 @@ def mutate_string(string, position, character):
     return string
 
 if __name__ == '__main__':
+
+
+
+
+#Solution 2: Convert the string to a list, then change the character at that position, then use .join method to join the string and return it
+
+def mutate_string(string, position, character):
+    l = list(string)
+    l[position] = character
+    string = ''.join(l)
+    return string
+
+if __name__ == '__main__':
